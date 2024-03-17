915fae9679b0ca91ed16a802f0c7294fc184d27e
Eleanor <eleanoryap15@gmail.com>
2024-03-17 18:37:01 +0800

    Deleted unused files, added a Components folder to store Assets and Search Bar folder. Changed the background and UI for search bar and search button.

---
+++ weatherapp/src/Components/Search/Search-bar.js
@@ -0,0 +1,34 @@
+import React, { useState} from "react";
+import './Search-bar.css' ;
+import { FaSearch } from "react-icons/fa";
+
+const Searchbar = ({onSearch}) => {
+    const [search, setSearch] = useState('');
+
+    const handleSearch = (e) => {
+      e.preventDefault();
+      onSearch(search);
+    };
+return (
+   <div className="container">
+        <form className="top-bar">
+            <input type="text" 
+            className="cityInput" 
+            placeholder ="Search for City or Country"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type="submit" className="searchIcon"><FaSearch/>
+                </button> 
+               
+        </form> 
+        {/* <div className="main-body">
+
+        </div> */}
+    </div>
+  
+);
+
+};
+
+export default Searchbar
